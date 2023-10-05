af99b53fa88da39f3677a6db8f3a21af03d39326
mdaseem03 <98540960+mdaseem03@users.noreply.github.com>
2023-10-05 16:33:00 +0530

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+pip install futures
+pip install pwntools
+pip install termcolor
+pip install requests
+
+cd tools
+git clone https://github.com/maurosoria/dirsearch.git
+cd dirsearch
+python3 dirsearch.py --version
+echo "Dirsearch Installed"
+
+
